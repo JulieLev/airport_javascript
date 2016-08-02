31aa19c22facc9dae4fed5d2e3ba9bd47bf4d575
JulieLev <jkgee1@yahoo.co.uk>
2016-08-02 17:42:29 +0100

    Planes takeoff and land without weather

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -13,9 +13,14 @@ describe('Airport', function(){
     expect(airport.planes()).toEqual([]);
   });
 
-  it('can clear planes for landing', function(){ 
+  it('can clear planes for landing', function(){
     airport.clearForLanding(plane);
     expect(airport.planes()).toEqual([plane]);
   });
 
+  it ('can clear planes for takeoff', function() {
+    airport.clearForLanding(plane);
+    airport.clearForTakeoff(plane);
+    expect(airport.planes()).toEqual([]);
+  });
 });
--- spec/FeatureSpec.js
+++ spec/FeatureSpec.js
@@ -14,5 +14,10 @@ describe('Feature Test:', function() {
     expect(airport.planes()).toContain(plane);
   });
 
+  it ('planes can be instructed to takeoff', function() {
+    plane.land(airport);
+    plane.takeoff();
+    expect(airport.planes()).not.toContain(plane);
+  });
 
 });
